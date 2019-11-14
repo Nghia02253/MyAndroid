efb7c7091b56572d343520ea8aaa2da6e8fe1e66
Nghia02253 <nghia02253@gmail.com>
2019-11-14 15:18:31 +0700

    Initial commit
    Signed-off-by: Nghia02253 <nghia02253@gmail.com>

--- app/src/main/java/com/nghia02253/myandroid/FragmentActivity.java
+++ app/src/main/java/com/nghia02253/myandroid/FragmentActivity.java
@@ -1,27 +1,41 @@
 package com.nghia02253.myandroid;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.Toast;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 public class FragmentActivity extends AppCompatActivity {
-    Button btnFragmentA, btnFragmentB;
-    FrameLayout frame_fragment;
+//    Button btnFragmentA, btnFragmentB;
+//    FrameLayout frame_fragment;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_activity);
-        btnFragmentA = findViewById(R.id.btnFragmentA);
-        btnFragmentB = findViewById(R.id.btnFragmentB);
-        frame_fragment = findViewById(R.id.frame_fragment);
+//        btnFragmentA = findViewById(R.id.btnFragmentA);
+//        btnFragmentB = findViewById(R.id.btnFragmentB);
+//        frame_fragment = findViewById(R.id.frame_fragment);
     }
 
-    public void LoadFragment(String fragmentName){
-        Toast.makeText(FragmentActivity.this, "Load Fragment", Toast.LENGTH_LONG).show();
+    public void LoadFragment(View view){
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        Fragment fragment = null;
+
+        switch (view.getId()) {
+            case R.id.btnFragmentA:
+                fragment = new Fragment_A();
+                break;
+            case  R.id.btnFragmentB:
+                fragment = new Fragment_B();
+                break;
+        }
+//        fragmentTransaction.replace(R.id.frame_fragment, fragment);
+        fragmentTransaction.commit();
     }
 }
